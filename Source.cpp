16de94fd4723734df7217ee16cd2ea9360f64168
Sogdiiana <119744104+Sogdiiana@users.noreply.github.com>
2022-12-09 22:14:58 +0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int x,y;// обмен значений
+	cout << "enter X -- ";
+	cin >> x;
+	cout  << "enter Y -- ";
+	cin >> y;
+	cout << "X= " << y;
+	cout << endl << "Y= " << x;
+	return 0;
+}
